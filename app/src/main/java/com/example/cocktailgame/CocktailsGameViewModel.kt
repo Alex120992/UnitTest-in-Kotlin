96848e2c2624908520566ebb90016e6bcc73ff79
Aleksey Zateev <alekseyzateev@gmail.com>
2020-11-29 02:15:07 +0500

    test app without UI

--- app/src/main/java/com/example/cocktailgame/CocktailsGameViewModel.kt
+++ app/src/main/java/com/example/cocktailgame/CocktailsGameViewModel.kt
@@ -12,11 +12,38 @@ class CocktailsGameViewModel
     private val questionLiveData = MutableLiveData<Question>()
     private val scoreLiveData = MutableLiveData<Score>()
 
-    fun getLoading ():LiveData<Boolean> = loadingLiveData
-    fun getError ():LiveData<Boolean> = errorLiveData
-    fun getQuestion ():LiveData<Question> = questionLiveData
-    fun getScore ():LiveData<Score> = scoreLiveData
+    fun getLoading(): LiveData<Boolean> = loadingLiveData
+    fun getError(): LiveData<Boolean> = errorLiveData
+    fun getQuestion(): LiveData<Question> = questionLiveData
+    fun getScore(): LiveData<Score> = scoreLiveData
+    private  var game:Game? = null
+    fun initGame() {
+        loadingLiveData.value = true
+        errorLiveData.value = false
+        factory
+            .buildGame(object : CocktailsGameFactory.CallBack {
+                override fun onSuccess(game: Game) {
+                    loadingLiveData.value = false
+                    errorLiveData.value = false
+                    scoreLiveData.value = game.score
+                    questionLiveData.value = game.nextQuestion()
+                    this@CocktailsGameViewModel.game = game
+                    nextQuestion()
+                }
 
+                override fun onError() {
+                    loadingLiveData.value = false
+                    errorLiveData.value = true
+                }
+
+            })
+    }
+
+    fun nextQuestion() {
+        game?.let {
+            questionLiveData.value = it.nextQuestion()
+        }
+    }
 
 
 }
--- app/src/test/java/com/example/cocktailgame/CocktailsGameViewModelUnitTests.kt
+++ app/src/test/java/com/example/cocktailgame/CocktailsGameViewModelUnitTests.kt
@@ -1,18 +1,17 @@
 package com.example.cocktailgame
 
-import android.arch.core.executor.TaskExecutor
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doAnswer
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class CocktailsGameViewModelUnitTests {
     @get:Rule
     val taskExecutorRule = InstantTaskExecutorRule()
+
     private lateinit var repository: CocktailsRepository
     private lateinit var factory: CocktailsGameFactory
     private lateinit var viewModel: CocktailsGameViewModel
@@ -43,10 +42,12 @@ class CocktailsGameViewModelUnitTests {
     }
 
     private fun setUpFactorySuccessGame(game: Game) {
-        doAnswer { val callBack: CocktailsGameFactory.CallBack = it.getArgument(0)
-        callBack.onSuccess(game)
+        doAnswer {
+            val callBack: CocktailsGameFactory.CallBack = it.getArgument(0)
+            callBack.onSuccess(game)
         }.whenever(factory).buildGame(any())
     }
+
     private fun setUpFactoryWithError() {
         doAnswer {
             val callback: CocktailsGameFactory.CallBack =
@@ -54,4 +55,83 @@ class CocktailsGameViewModelUnitTests {
             callback.onError()
         }.whenever(factory).buildGame(any())
     }
+
+    @Test
+    fun init_shouldBuildGame() {
+        viewModel.initGame()
+        verify(factory).buildGame(any())
+    }
+
+    @Test
+    fun init_shouldShowLoading(){
+        viewModel.initGame()
+
+        verify(loadingObserver).onChanged(eq(true))
+    }
+    @Test
+    fun init_shouldHideErroe(){
+        viewModel.initGame()
+
+        verify(errorObserver).onChanged(eq(false))
+    }
+    @Test
+    fun init_shouldShowError_whenFactoryReturnsError() {
+        setUpFactoryWithError()
+        viewModel.initGame()
+        verify(errorObserver).onChanged(eq(true))
+    }
+    @Test
+    fun init_shouldHideLoading_whenFactoryReturnsError() {
+        setUpFactoryWithError()
+        viewModel.initGame()
+        verify(loadingObserver).onChanged(eq(false))
+    }
+
+    @Test
+    fun init_shouldHideError_whenFactoryReturnsSuccess() {
+        setUpFactorySuccessGame(game)
+        viewModel.initGame()
+        verify(errorObserver, times(2)).onChanged(eq(false))
+    }
+
+    @Test
+    fun init_shouldHideLoading_whenFactoryReturnsSuccess() {
+        setUpFactorySuccessGame(game)
+        viewModel.initGame()
+        verify(loadingObserver).onChanged(eq(false))
+    }
+
+    @Test
+    fun init_shouldShowScore_whenFactoryReturnsSuccess() {
+        val score = mock<Score>()
+        whenever(game.score).thenReturn(score)
+        setUpFactorySuccessGame(game)
+        viewModel.initGame()
+        verify(scoreObserver).onChanged(eq(score))
+    }
+
+    @Test
+    fun init_shouldShowFirstQuestion_whenFactoryReturnsSuccess() {
+        val question = mock<Question>()
+        whenever(game.nextQuestion()).thenReturn(question)
+        setUpFactorySuccessGame(game)
+        viewModel.initGame()
+        verify(questionObserver).onChanged(eq(question))
+    }
+
+    @Test
+    fun nextQuestion_shouldShowQuestion(){
+        val question1 = mock<Question>()
+        val question2 = mock<Question>()
+        whenever(game.nextQuestion()).thenReturn(question1).thenReturn(question2)
+        setUpFactorySuccessGame(game)
+        viewModel.initGame()
+        viewModel.nextQuestion()
+        verify(questionObserver).onChanged(eq(question2))
+
+    }
+
+
+
+
 }
